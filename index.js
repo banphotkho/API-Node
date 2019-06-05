11d2f17e5fe5feb60c7a2fd9a3f3311dc561fe7a
Banphot Kongpom <banphot.kon@tokiomarinelife.co.th>
2019-06-05 15:22:13 +0700

    Update callback process

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api',customer);
 app.use('/api/dashboard',dashboard);
 
+// test acall back
+console.log('Before');
+const user = getUse(1,function(user){
+    console.log('Get user:',user);
+    getUserrule(user.id,(rule)=>{
+        console.log('Get user rule',rule);
+    });
+});
+console.log('After');
+
+
+function getUse(id, callback){
+    setTimeout(() => {
+        console.log('Reading user form a database...');
+        callback( {id:id, gitHubusername: 'banphot'});
+    }, 2000);
+};
+
+//create function to return roll
+function getUserrule(id,callback) {
+    setTimeout(() => {
+            console.log('Return user role');
+            callback(['admin','user','super user']);
+    }, 2000);
+}
+
+
+
+
+
 //set port for running
 app.get('/',(req,res)=>{
     res.send('Hello My API');
